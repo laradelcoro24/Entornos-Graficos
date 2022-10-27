280b9cc85df800605e19c60200ff57fce6864bd4
Lara Del Coro <laradelcoro01@gmail.com>
2022-10-27 16:24:27 -0300

    Practica 8

---
+++ Practica 7-PHP/ej5.2.php
@@ -0,0 +1,21 @@
+<?php
+session_start();
+?>
+<html>
+
+<head>
+<meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 5</title>
+</head>
+
+<body>
+    <?php
+    echo "Nombre de usuario recuperado de la variable de sesión:" . $_SESSION['usuario'];
+    echo "<br><br>";
+    echo "La clave recuperada de la variable de sesión:" . $_SESSION['clave'];
+    ?>
+</body>
+
+</html>
